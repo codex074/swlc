02c7902e46a6d26d5d06a9ceab21594a5d33367f
codex074 <trd.a.titans@gmail.com>
2026-02-08 21:01:49 +0700

    Change audio button icon to speaker emoji

--- src/components/PatientLabel.jsx
+++ src/components/PatientLabel.jsx
@@ -90,9 +90,7 @@ export default function PatientLabel({ schedule }) {
             default:
                 return (
                     <>
-                        <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-2.236 9.168-5.518" />
-                        </svg>
+                        <span className="text-2xl mr-2">🔊</span>
                         <span>ฟังวิธีทานยา</span>
                     </>
                 );
